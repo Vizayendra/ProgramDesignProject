e78cf66142c3d76ef57ae8d3b921631b9a7f39ed
Bearlikehoney <zulfadhli0455@gmail.com>
2024-12-20 00:56:05 +0800

    print welcome to wordle

--- main.py
+++ main.py
@@ -7,3 +7,4 @@
 # IDs: MEMBER_ID_1 | MEMBER_ID_2 | MEMBER_ID_3 | MEMBER_ID_3 
 # Emails: MEMBER_EMAIL_1 | MEMBER_EMAIL_2 | MEMBER_EMAIL_3 | MEMBER_EMAIL_3 
 # *************************************************************************
+print('Welcome to wordle !!!!')
